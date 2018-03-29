e70358abdade6c2e150ff6b5169cb72525881041
Alfred V Jose <alfredzeee@gmail.com>
2018-03-29 14:12:52 +0530

    Adding Facebook Authentication

---
+++ frontend/firebase.js
@@ -0,0 +1,14 @@
+
+
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmErNRB_6zuLdf-MTk03Fa0i1Dx5S-ltM ",
+  authDomain: "snapdiet-alpha.firebaseapp.com",
+  databaseURL: "https://snapdiet-alpha.firebaseio.com",
+  storageBucket: "snapdiet-alpha.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
